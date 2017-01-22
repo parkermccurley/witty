cd87cc41e1de7abdde26acb4079e54ae25e0b8ec
Parker <parkermccurlz@gmail.com>
2017-01-22 01:37:34 -0500

    Update package.json

--- src/app/index.js
+++ src/app/index.js
@@ -1,11 +1,14 @@
 #!/usr/bin/env node
 
+import commander from 'commander'
 import * as twitter from './services/twitter.service'
 import { tweetToJSON } from './helpers/tweet.helper'
 import constants from './core'
 
 const query = 'parker'
 const count = 5
+
+console.log("Hello, world!");
 
 twitter.search(query, count, constants.bearerToken)
   .then(response => {
